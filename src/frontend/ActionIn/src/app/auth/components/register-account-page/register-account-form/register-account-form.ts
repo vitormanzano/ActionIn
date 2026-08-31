1a3cc5609a16c0ee8b227112dd7ecdff5dc954d9
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-08-31 13:55:22 -0300

    feat: Register account page
    If an visitor wants to use the application, he should first register a
    new account
    
    Just the structure of the register account page, without any feature.
    Note: Create a component form that should be reused in the login and
    register page.

--- src/frontend/ActionIn/src/app/auth/components/register-account-page/register-account-form/register-account-form.ts
+++ src/frontend/ActionIn/src/app/auth/components/register-account-page/register-account-form/register-account-form.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   selector: 'app-register-account-form',
   styleUrl: './register-account-form.css',
   templateUrl: './register-account-form.html',
 })
-export class RegisterAccountForm {}
+export class RegisterAccountForm {
+  form = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  onSubmit() {}
+}
--- src/frontend/ActionIn/src/app/auth/components/register-account-page/register-account-page.ts
+++ src/frontend/ActionIn/src/app/auth/components/register-account-page/register-account-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../shared/header/header';
+import { RegisterAccountForm } from './register-account-form/register-account-form';
 
 @Component({
-  imports: [Header],
+  imports: [Header, RegisterAccountForm],
   selector: 'app-register-account-page',
   styleUrl: './register-account-page.css',
   templateUrl: './register-account-page.html',
