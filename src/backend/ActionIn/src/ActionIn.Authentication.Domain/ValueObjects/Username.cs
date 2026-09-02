4023bc88380403734ea5ab05b30f5084ed5c8cd2
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-09-02 20:30:50 -0300

    feat: Value Object Username for the Account class

---
+++ src/backend/ActionIn/src/ActionIn.Authentication.Domain/ValueObjects/Username.cs
@@ -0,0 +1,25 @@
+namespace ActionIn.Authentication.Domain.ValueObjects;
+
+public record Username
+{
+    private string Value { get; private set; } = string.Empty;
+
+    public Username(string username)
+    {
+        SetUsername(username);
+    }
+
+    public void SetUsername(string username)
+    {
+        ValidateUsername(username);
+        Value = username;
+    }
+
+    private void ValidateUsername(string username)
+    {
+        if (username is null)
+            throw new Exception("Username cannot be null"); // Use notifications + result pattern
+        if (username.Length < 4)
+            throw new Exception("Username must be at least 4 characters");
+    }
+}
